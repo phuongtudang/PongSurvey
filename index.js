e734d651ebd7c4c064196ff0568214716df502c4
Phuong Dang <phuongdang@admins-MacBook-Air-2.local>
2017-09-19 16:44:15 +0700

    change greeting

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send({ hi: 'there' })
-})
+app.get("/", (req, res) => {
+  res.send({ hi: "beautiful!" });
+});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
